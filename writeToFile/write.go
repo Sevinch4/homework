5311a071b2ea2d89c69876c8bc7b0b63229ee9f5
Sevinch4 <nsevinch3@gmail.com>
2023-12-08 22:20:34 +0500

    file write

---
+++ writeToFile/write.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var m = map[string]string{
+	"name":   "ism",
+	"age":    "yosh",
+	"hello":  "salom",
+	"home":   "uy",
+	"school": "maktab",
+	"try":    "harakat",
+	"horse":  "ot",
+	"monkey": "maymun",
+	"dog":    "kuchuk",
+	"cat":    "mushuk",
+}
+
+func main() {
+	file, err := os.OpenFile("File.txt", os.O_CREATE|os.O_RDWR, 06666)//openFile is open file or create file 
+	if err != nil {
+		panic(err)
+	}
+	//file write I
+	// n, err := file.WriteString("hello world")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(n)
+
+	//file write II
+	
+
+	// file, err := os.OpenFile("file.txt", os.O_RDWR|os.O_CREATE, 0666)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+	// _, err = file.WriteString("english - uzb\n\n")
+	// if err != nil{
+	// 	panic(err)
+	// }
+	// c := 0
+	// for i, v := range m {
+	// 	c++
+	// 	v += "\n"
+	// 	str := fmt.Sprintf("%d. %s - %s",c,i,v)
+	// 	if _, err := io.WriteString(file, str); err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+}
